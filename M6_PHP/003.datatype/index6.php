69df011c9a5777c05675049537815e6caf96232f
huong <tieuanvenus@gmai.com>
2019-07-01 19:12:07 +0700

    json

---
+++ M6_PHP/003.datatype/index6.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    Kiểu null
+</pre>
+<?php
+$x=null;
+
+$y;
+var_dump($x);
+var_dump($y);
+?>
+</body>
+</html>
